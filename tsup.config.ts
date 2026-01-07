adde562604c108adab20ac83cf41a3338e7d6544
daniel <daniel@speakeasy.com>
2026-01-07 22:48:18 +0000

    chore: build with tsup

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["index.ts"],
+  format: ["esm", "cjs"],
+  dts: true,
+  clean: true,
+  splitting: false,
+  sourcemap: true,
+  minify: false,
+  treeshake: true,
+});
